c5bc4926516296e29d8b06a56a77a72db97eee26
VADLAKONDA VINAY GOUD <95540440+Vinaygoud74@users.noreply.github.com>
2022-01-25 23:00:14 +0530

    Add files via upload

---
+++ Greaterornot.js
@@ -0,0 +1,13 @@
+var a = 40;
+if(a>10)
+{
+console.log("GIVEN NUMBER IS GREATER THAN 10");
+}
+else if(a<10)
+{
+console.log("GIVEN NUMBER IS LESS THAN 10")
+}
+else
+{
+console.log("GIVEN NUMBER IS EQUAL TO 10");
+}
