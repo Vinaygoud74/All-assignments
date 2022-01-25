7ec7de31c2702f99662bdea470fcef79c1a66e27
VADLAKONDA VINAY GOUD <95540440+Vinaygoud74@users.noreply.github.com>
2022-01-25 23:01:56 +0530

    Add files via upload

---
+++ MAXFROM3.js
@@ -0,0 +1,15 @@
+var a = 10;
+var b = 200;
+var c = 30;
+if(a>b && a>c)
+{
+console.log("VALUE OF A IS MAXIMUM");
+}
+else if(b>c && b>a)
+{
+console.log("VALUE OF Bs IS MAXIMUM");
+}
+else
+{
+console.log("VALUE OF C IS MAXIMUM")
+}
